fdf23c11d5141a87fab6597ae7dc960834c1c635
Rom Jr. <157264890+RomNeedBoba@users.noreply.github.com>
2026-03-24 10:07:06 +0700

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../auth/token";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "backend.khtts.me",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://backend.khtts.me",
 });
 
 // Attach Authorization header
